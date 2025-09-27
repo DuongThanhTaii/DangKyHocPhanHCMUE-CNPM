dc965b84981f8ee048a3e3e63e1b04c7e8555d89
minhthinh145 <minhthinh0384360149@gmail.com>
2025-09-27 21:03:51 +0700

    fix(redux): add api reducerpath

--- apps/frontend/src/app/store.ts
+++ apps/frontend/src/app/store.ts
@@ -15,11 +15,16 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux"; // 👈 type-only
+import type { TypedUseSelectorHook } from "react-redux";
 import authReducer from "../features/auth/authSlice";
-
-export const store = configureStore({ reducer: { auth: authReducer } });
-export type RootState = ReturnType<typeof store.getState>;
+import { api } from "./api"
+export const store = configureStore({
+    reducer: {
+        [api.reducerPath]: api.reducer,
+        auth: authReducer,
+    },
+    middleware: (gDM) => gDM().concat(api.middleware),
+}); export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
