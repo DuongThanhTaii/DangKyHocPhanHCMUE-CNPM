d31977c2f5338d39d6e09cd918b48da87e72c6f5
minhthinh145 <minhthinh0384360149@gmail.com>
2025-09-27 20:40:47 +0700

    chore: Change API_URL link

--- apps/frontend/src/app/api.ts
+++ apps/frontend/src/app/api.ts
@@ -4,7 +4,7 @@ import type { RootState } from "./store";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    baseUrl: import.meta.env.VITE_API_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) headers.set("authorization", `Bearer ${token}`);
