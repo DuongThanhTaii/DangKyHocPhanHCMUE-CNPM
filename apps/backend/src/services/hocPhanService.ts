1a609d3d1b4318e9e26672f7852f5797e548b710
minhthinh145 <minhthinh0384360149@gmail.com>
2025-10-24 20:47:05 +0700

    feat: add service - router for giangVien

--- apps/backend/src/services/hocPhanService.ts
+++ apps/backend/src/services/hocPhanService.ts
@@ -5,7 +5,7 @@ export class HocPhanService {
     private uow = UnitOfWork.getInstance();
 
     /**
-     * Lấy danh sách học phần để PDT tạo lớp
+     * Lấy danh sách học phần để TLK tạo lớp
      */
     async getHocPhansForCreateLop(
         hocKyId: string
@@ -14,8 +14,11 @@ export class HocPhanService {
             const hocPhans = await this.uow.hocPhanRepository.findForCreateLop(hocKyId);
 
             const dtos: HocPhanForCreateLopHocPhanDTO[] = hocPhans.map((hp) => {
+                // ✅ Lấy giảng viên từ de_xuat_hoc_phan
                 const deXuat = hp.mon_hoc.de_xuat_hoc_phan?.[0];
                 const tenGiangVien = deXuat?.giang_vien?.users?.ho_ten || "Chưa phân công";
+                const giangVienId = deXuat?.giang_vien?.id
+
 
                 return {
                     id: hp.id,
@@ -24,6 +27,7 @@ export class HocPhanService {
                     soTinChi: hp.mon_hoc.so_tin_chi,
                     soSinhVienGhiDanh: hp._count.ghi_danh_hoc_phan,
                     tenGiangVien,
+                    giangVienId,
                 };
             });
 
