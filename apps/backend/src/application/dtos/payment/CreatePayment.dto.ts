24bb8b187618950b1cce4da2f0a8bb31b54692aa
minhthinh145 <minhthinh0384360149@gmail.com>
2025-10-31 11:55:06 +0700

    feat: add dtos for payment

---
+++ apps/backend/src/application/dtos/payment/CreatePayment.dto.ts
@@ -0,0 +1,13 @@
+export interface CreatePaymentRequestDTO {
+    orderId: string;
+    amount: number;
+    orderInfo: string;
+    redirectUrl: string;
+    ipnUrl: string;
+}
+
+export interface CreatePaymentResponseDTO {
+    payUrl: string;
+    orderId: string;
+    requestId: string;
+}
