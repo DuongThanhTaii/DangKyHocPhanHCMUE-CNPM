82a1a8785cb1f9cd532c0c61f846f031202ec91d
minhthinh145 <minhthinh0384360149@gmail.com>
2025-10-31 11:59:46 +0700

    feat: add routes for server

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,4 +1,8 @@
 import "dotenv/config";
 import app from "./app";
+import tuitionRoutes from "./interface/routes/tuition.routes";
+import paymentRoutes from "./interface/routes/payment.routes"; // add this import
 const PORT = process.env.PORT || 3000;
+app.use("/api/sv/hoc-phi", tuitionRoutes);
+app.use("/api/payment", paymentRoutes); // mount payment routes
 app.listen(PORT, () => console.log(`API running at http://localhost:${PORT}`));
