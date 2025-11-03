e3e4fb9eac86da8f4d181802a8b625c390a424c6
DuongThanhTaii <duongthanhtai13@gmail.com>
2025-11-03 12:04:15 +0700

    hot fix fe

--- apps/frontend/src/pages/pdt/components/PhaseConfigBase.tsx
+++ apps/frontend/src/pages/pdt/components/PhaseConfigBase.tsx
@@ -296,11 +296,7 @@ export const PhaseConfigBase = forwardRef<PhaseConfigRef, PhaseConfigBaseProps>(
               <>Lưu và xem</>
             ) : (
               <>
-                {/* Icon Bút chì (Edit) */}
-                <svg /* ...thêm các thuộc tính SVG ở trên vào đây... */>
-                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                </svg>
+              
                 Chỉnh sửa
               </>
             )}
